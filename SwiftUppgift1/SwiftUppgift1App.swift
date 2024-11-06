d63c929bd9221d656b66a5ce3bbad89417d2d386
EleniVrabec <eleni.vrabec@gritacademy.se>
2024-11-06 12:30:20 +0100

    Initial Commit

---
+++ SwiftUppgift1/SwiftUppgift1App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUppgift1App.swift
+//  SwiftUppgift1
+//
+//  Created by Eleni Vrabec on 2024-11-06.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUppgift1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
