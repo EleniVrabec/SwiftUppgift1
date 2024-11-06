828bcbe6e90578e48dec929b0c47841a0bdd748e
EleniVrabec <eleni.vrabec@gritacademy.se>
2024-11-06 13:10:00 +0100

    Uppgift 1

--- SwiftUppgift1/ContentView.swift
+++ SwiftUppgift1/ContentView.swift
@@ -10,13 +10,51 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack(alignment: .top, spacing: 0.0){
+                
+                Text("")
+                    
+                    .frame(maxWidth: .infinity, maxHeight: 200)
+                    .background(Color.red)
+
+                
+                Text("")
+                    
+                    .frame(maxWidth: .infinity, maxHeight: 200)
+                    .background(Color.green)
+
+                Text("")
+                   
+                    .frame(maxWidth: .infinity, maxHeight: 200)
+                    .background(Color.yellow)
+
+                
+            }
+            HStack{
+                
+            }.frame(maxWidth: .infinity, maxHeight: 150)
+                .background(Color.gray)
+            
+            HStack{
+                
+            }.frame(maxWidth:100, maxHeight: 100)
+                .background(Color.white)
+            Spacer()
+            HStack{
+                Spacer()
+                    
+                Text("")
+                    .frame(width: 50.0, height:50.0)
+                    .background(Color.black)
+            }
+            .padding(.trailing, 20.0)
+            .frame(maxWidth: .infinity, maxHeight: 100)
+            .background(Color.red)
+
+            
         }
-        .padding()
-    }
+        .frame(maxHeight: .infinity)
+        .background(Color.blue)    }
 }
 
 #Preview {
